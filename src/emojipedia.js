5e311f3189d832615cc615977245802d8bd946ff
Eva Cebrian <65074646+Eva-Cebrian@users.noreply.github.com>
2024-03-16 12:23:47 +0100

    Proyecto Emojis

---
+++ src/emojipedia.js
@@ -0,0 +1,31 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id:4,
+    emoji: "🥰",
+    name: "Happy",
+    meaning: "This adorable 🥰 emoji represents a smiling face surrounded by hearts, embodying infatuation and romance. It's perfect for expressing affection, love and romantic affairs of the heart while floating on cloud nine. Deep connection and loving feelings can be conveyed to the person you are talking to through this emoji. It can also be used to express appreciation for special moments or heartfelt gestures. All in all, this emoji is a wonderful way to express your love and affection in digital communication."
+  }
+];
+
+export default emojipedia;
